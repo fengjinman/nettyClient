d272f6187f5f099c5bbd4b1d51cdbd4c7cb52b87
fengjinman@empowerwin.com <fengjinman>
2018-09-12 17:55:26 +0800

    netty的测试客户端代码

---
+++ src/main/java/client2/MyChatClientHandler.java
@@ -0,0 +1,23 @@
+package client2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class MyChatClientHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(msg);
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] data = new byte[buf.readableBytes()];
+        buf.readBytes(data);
+        String request = new String(data, "utf-8");
+        System.out.println("Client1: " + request);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
